2d30fb170f3eb83f8c8257b00273546427003c16
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-11 02:28:53 +0300

    return large array

---
+++ Day05return_largest_number_in_arrays.js
@@ -0,0 +1,24 @@
+/**
+ * Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+ */
+
+function largestOfFour(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+  //get first value from each array
+      let largestNumber = arr[i][0];
+  //loop to compare with the remaining values
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNumber) {
+          largestNumber = arr[i][j];
+        }
+      }
+      results[i] = largestNumber;
+    }
+  
+    return results;
+  }
+  
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));

a5aa9917794f250a4d2bbe017a0f4d460fe2dfc8
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-12 13:41:27 +0300

    confirm the ending

---
+++ Day06confirm_the_ending.js
@@ -0,0 +1,11 @@
+/**
+ * Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+ */
+
+function confirmEnding(str, target) {
+    return str.slice(str.length - target.length) === target;
+ }
+ 
+ confirmEnding("Bastian", "n");

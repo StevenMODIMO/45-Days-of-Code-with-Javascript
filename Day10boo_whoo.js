609e2639d471f697da030b3d3d26791369d6102c
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-16 08:16:24 +0300

    boo who

---
+++ Day10boo_whoo.js
@@ -0,0 +1,10 @@
+/**
+* Check if a value is classified as a boolean primitive. Return true or false.
+* Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+    return typeof(bool) === 'boolean'
+}
+
+console.log(booWho(false))

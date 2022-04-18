6f40d46243ac9f0822d3dd577879528ce31a95eb
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-18 07:59:22 +0300

    slice_and_splice

---
+++ Day12slice-and_splice.js
@@ -0,0 +1,20 @@
+/**
+ * You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+ */
+
+function frankenSplice(arr1, arr2, n) {
+    let newarr2 = arr2.slice();
+    for(let i = 0; i < arr1.length; i++) {
+      newarr2.splice(n, 0, arr1[i]);
+      n++
+    }
+    return newarr2;
+  }
+  
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));

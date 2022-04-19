740380be48de7f7b0205991a0849b90481e80596
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-19 08:34:26 +0300

    falsy_bouncer

---
+++ Day13falsy_bouncer.js
@@ -0,0 +1,13 @@
+/**
+ * Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+ */
+
+function bouncer(arr) {
+    return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);

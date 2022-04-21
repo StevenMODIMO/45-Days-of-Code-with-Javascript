914e1a30e993a6a769edb6cd4ceff89ebac7f54e
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-04-21 10:37:55 +0300

    mutations

---
+++ Day15mutations.js
@@ -0,0 +1,22 @@
+/**
+ * Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+
+ */
+
+function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+}
+
+console.log(mutation(["hello", "hey"]))

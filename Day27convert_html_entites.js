f46d5e82ae3e3b109b611f579b3cb40abb6e2d09
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-05-03 09:36:27 +0300

    convert html entities

---
+++ Day27convert_html_entites.js
@@ -0,0 +1,39 @@
+/**
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+
+ */
+
+ function convertHTML(str) {
+    // Split by character to avoid problems.
+  
+    var temp = str.split("");
+  
+    // Since we are only checking for a few HTML elements, use a switch
+  
+    for (var i = 0; i < temp.length; i++) {
+      switch (temp[i]) {
+        case "<":
+          temp[i] = "&lt;";
+          break;
+        case "&":
+          temp[i] = "&amp;";
+          break;
+        case ">":
+          temp[i] = "&gt;";
+          break;
+        case '"':
+          temp[i] = "&quot;";
+          break;
+        case "'":
+          temp[i] = "&apos;";
+          break;
+      }
+    }
+  
+    temp = temp.join("");
+    return temp;
+  }
+  
+  //test here
+  convertHTML("Dolce & Gabbana");

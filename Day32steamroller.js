52d9ef37e913fdc0e5d064f8f4bcc230f1896ad2
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-05-08 21:17:42 +0300

    steam roller

---
+++ Day32steamroller.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * Flatten a nested array. You must account for varying levels of nesting.
+ */
+
+ function steamrollArray(arr) {
+    const flattenedArray = [];
+    // Loop over array contents
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        // Recursively flatten entries that are arrays
+        //  and push into the flattenedArray
+        flattenedArray.push(...steamrollArray(arr[i]));
+      } else {
+        // Copy contents that are not arrays
+        flattenedArray.push(arr[i]);
+      }
+    }
+    return flattenedArray;
+  };
+  
+  // test here
+  steamrollArray([1, [2], [3, [[4]]]]);

5aa1b99ec78f99aee16e6b1686b51e77c4094b90
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-05-09 13:12:32 +0300

    binary agents

---
+++ Day33binary_agents.js
@@ -0,0 +1,23 @@
+/**
+ * Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
+
+ 
+ */
+
+function binaryAgent(str) {
+  var newBin = str.split(" ");
+  var binCode = [];
+
+  for (let i = 0; i < newBin.length; i++) {
+    binCode.push(String.fromCharCode(parseInt(newBin[i], 2)));
+  }
+  return binCode.join("");
+}
+
+console.log(
+  binaryAgent(
+    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+  )
+);

150ce622faa8f8a4cfeb73c22550aa6d2cc917b3
StevenMODIMO <81316521+StevenMODIMO@users.noreply.github.com>
2022-05-10 17:12:42 +0300

    everything be true

---
+++ Day34everything_be_true.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+ */
+
+function truthCheck(collection, pre) {
+    return collection.every(function (element) {
+      return element.hasOwnProperty(pre) && Boolean(element[pre]);
+    });
+  }
+  
+  truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "isBot")
